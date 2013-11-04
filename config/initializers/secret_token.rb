6dc7661a5b8a3629217ef79aaf700409229a9617
Brian Varkevisser <baugvark@yahoo.com>
2013-11-04 15:33:33 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campaigner::Application.config.secret_token = '9c0a9236ef9a730f19f092838bf22047e9d7f0a50d92bb5a091b8b816f90abd11dc6a8575de4676a10e4c4ba0f5b10bad70a7a748fb99d2a345931930b7a6141'
